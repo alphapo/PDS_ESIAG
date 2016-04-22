e86b3bf63c0dae1796812b999df0d6f73214fc8d
Alpha Ibrahima BARRY <alpha.ibra.barry@outlook.com>
2016-04-22 21:49:45 +0200

    initialisation de la restitution d'une simulation

---
+++ rendu_R_3/client_package/client/tools/BuilderRestitution.java
@@ -0,0 +1,15 @@
+package client.tools;
+
+import java.util.Collection;
+
+import javax.swing.JPanel;
+
+public class BuilderRestitution {
+	
+	static void remindSimulatorParameters(JPanel contenair, Collection informations){}
+	
+	static void drawArray(JPanel contenair, Collection informations){}
+	
+	static void drawGraph(JPanel contenair, Collection informations){}
+	
+}
