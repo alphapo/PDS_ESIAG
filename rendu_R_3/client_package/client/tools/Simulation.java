65f5006954594cc965b92f667053efc7e586ce87
Alpha Ibrahima BARRY <alpha.ibra.barry@outlook.com>
2016-05-09 09:33:27 +0200

    type de remboursement: annuité constant tableau d'amortissement OK
    @bAlphaIbrahima

--- rendu_R_3/client_package/client/tools/Simulation.java
+++ rendu_R_3/client_package/client/tools/Simulation.java
@@ -8,19 +8,39 @@ import javax.swing.table.AbstractTableModel;
 public class Simulation extends AbstractTableModel {
 	private final List<TempoData> data = new ArrayList<TempoData>();
 
-	private final String[] entetes = {"Numéro", "Mensualité","Taux d'interet","Assurance","Capital restant dû"};
+	private final String[] entetes = {"Numéro", "Mensualité", "Capital Amorti","Interets","Assurance","Capital restant dû"};
 
-	public Simulation(double amount, double rate, double duration, int insurance) {
+	/*
+	 * 
+	 * Method of reimbursement: constant annual installment
+	 * 
+	 * 
+	 */
+	public Simulation(double amount, double rate, double duration, double insurance) {
 		
-		double amountTTC = amount + amount*rate + insurance;
-		System.out.println(amountTTC);
-		double monthly= amountTTC/duration;
-		double amountRemaining =  amountTTC-monthly;
+		double remaining = amount;
+		double monthly ;
+		double capital;
+		double interest;
+		double rateByMonth = (rate/12);
+		double totalCapital = 0;
+		double totalInterest = 0;
+		double totalMonthly = 0;
 		
 		for(int i = 1; i<=duration; i++){
-			amountRemaining = amountRemaining-monthly;
-			data.add(new TempoData(i, amount, rate, duration, insurance, monthly, amountRemaining));
+			interest = keep2numbersAfterComma(remaining*rateByMonth);
+			capital = keep2numbersAfterComma(remaining*rateByMonth/(Math.pow((1 + rateByMonth),duration-(i-1)) - 1));
+			monthly = keep2numbersAfterComma(capital + interest + insurance);
+			remaining = keep2numbersAfterComma(remaining-capital);
+			
+			data.add(new TempoData(Integer.toString(i), monthly, capital, interest , insurance, remaining));
+			
+			totalCapital = totalCapital + capital;
+			totalInterest = totalInterest + interest;
+			totalMonthly = totalMonthly + monthly;
 		}
+		double totalInsurance = insurance*duration;
+		data.add(new TempoData("Total", keepNoneNumbersAfterComma(totalMonthly), keepNoneNumbersAfterComma(totalCapital), keepNoneNumbersAfterComma(totalInterest), totalInsurance, 0 ));
 	}
 
 	public int getRowCount() {
@@ -42,14 +62,25 @@ public class Simulation extends AbstractTableModel {
          case 1:
              return data.get(rowIndex).getMonthly();
          case 2:
-             return data.get(rowIndex).getRate();
+             return data.get(rowIndex).getCapitalAmorti();
          case 3:
-             return data.get(rowIndex).getInsurance();
+             return data.get(rowIndex).getInterest();
          case 4:
+             return data.get(rowIndex).getInsurance();
+         case 5:
              return data.get(rowIndex).getRemaining();
          default:
              return null; 
      }
 	}
-
+	private static double keep2numbersAfterComma(double number){
+		//round up number
+		number = Math.round(number * Math.pow(10,1)) / Math.pow(10,1);
+		//keep 2 numbers after comma
+		return (double)((int)(number*100))/100;
+	}
+	private static double keepNoneNumbersAfterComma(double number){
+		//round up number
+		return Math.round(number * Math.pow(10,0)) / Math.pow(10,0);
+	}
 }
--- rendu_R_3/client_package/client/tools/TempoData.java
+++ rendu_R_3/client_package/client/tools/TempoData.java
@@ -1,85 +1,46 @@
 package test2;
 
 public class TempoData {
-	int number;
-	double amount;
-	double rate;
-	double duration;
+	String number;
 	double monthly;
 	double interest;
 	double insurance;
 	double remaining;
+	double capitalAmorti;
 	
-	public TempoData(int number, double amount, double rate, double duration, int insurance, double monthly, double remaining) {
+	public TempoData(String number, double monthly, double capitalAmorti, double interest, double insurance, double remaining ) {
 		this.number = number;
-		this.amount = amount;
-		this.rate = rate;
-		this.duration = duration;
 		this.monthly = monthly; 
 		this.insurance = insurance;
-		interest = (int)(amount*rate)/duration;
+		this.interest = interest;
 		this.remaining = remaining;
+		this.capitalAmorti = capitalAmorti;
 	}
 
-	public int getNumber() {
+	public String getNumber() {
 		return number;
-	}
-
-	public void setNumber(int number) {
-		this.number = number;
-	}
-
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-
-	public double getRate() {
-		return rate;
-	}
-
-	public void setRate(double rate) {
-		this.rate = rate;
-	}
-
-	public double getDuration() {
-		return duration;
-	}
-
-	public void setDuration(int duration) {
-		this.duration = duration;
 	}
 
 	public double getMonthly() {
 		return monthly;
 	}
 
-	public void setMonthly(int monthly) {
-		this.monthly = monthly;
-	}
-
 	public double getInterest() {
 		return interest;
-	}
-
-	public void setInterest(int interest) {
-		this.interest = interest;
 	}
 
 	public double getInsurance() {
 		return insurance;
 	}
 
-	public void setInsurance(int insurance) {
-		this.insurance = insurance;
+	public double getRemaining() {
+		return remaining;
 	}
 
-	public double getRemaining() {
-		// TODO Auto-generated method stub
-		return  remaining;
+	public double getCapitalAmorti() {
+		return capitalAmorti;
 	}
+
+	
 	
 }
