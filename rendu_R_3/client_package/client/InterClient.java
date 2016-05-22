d4cb140312c75ed04c1f8f6e31b64d6bd68f8a7e
abda123 <vybzbalde@gmail.com>
2016-05-22 20:09:53 +0200

    interface Client+UC restitution

--- rendu_R_3/client_package/client/InterClient.java
+++ rendu_R_3/client_package/client/InterClient.java
@@ -8,6 +8,10 @@ import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.text.*;
 
+import client.beans.Frequency;
+import client.beans.LoanRepaymentPlan;
+import client.beans.ShowArrayRestitution;
+
 import java.beans.PropertyChangeListener;
 import java.sql.*;
 import java.sql.DriverManager;
@@ -24,6 +28,8 @@ import java.awt.Font;
 import java.awt.TextField;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.awt.event.ActionEvent;
 
 public class InterClient extends JFrame {
@@ -75,12 +81,11 @@ public class InterClient extends JFrame {
 		JComboBox comboBoxLoanType = new JComboBox();
 		comboBoxLoanType.setBounds(306, 182, 119, 20);
 		contentPane.add(comboBoxLoanType);
-
 		try{
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/test_pds","root","");
 			statement=connection.createStatement();
 			result=statement.executeQuery("select * from loantype");
-			
+
 			while( result.next()){
 				comboBoxLoanType.addItem(result.getString("name"));
 			}
@@ -92,7 +97,6 @@ public class InterClient extends JFrame {
 			System.out.println(e);
 		}
 	
-
 		JFormattedTextField amountTextField = new JFormattedTextField();
 		amountTextField.addKeyListener(new KeyAdapter() {
 			@Override
@@ -111,59 +115,74 @@ public class InterClient extends JFrame {
 		amountTextField.setBounds(306, 66, 119, 20);
 		contentPane.add(amountTextField);
 
-		System.out.println(amountTextField.getText());
-
-
-
-
 		JButton btnSimuler = new JButton("Simuler");
 		btnSimuler.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnSimuler.setBounds(442, 302, 89, 39);
 		contentPane.add(btnSimuler);
-		
+
 		Label frenquency = new Label("Fr\u00E9quence de remboursement");
 		frenquency.setFont(new Font("Dialog", Font.BOLD, 12));
 		frenquency.setAlignment(Label.CENTER);
 		frenquency.setBounds(106, 230, 178, 22);
 		contentPane.add(frenquency);
-		
+
 		JComboBox frequencyCombobox = new JComboBox();
 		frequencyCombobox.setModel(new DefaultComboBoxModel(new String[] {"Mensuel", "Annuel"}));
 		frequencyCombobox.setBounds(306, 232, 119, 20);
 		contentPane.add(frequencyCombobox);
-		
+
+
 		JComboBox durationComboBox = new JComboBox();
 		durationComboBox.setModel(new DefaultComboBoxModel());
 		durationComboBox.setBounds(306, 115, 119, 20);
 		contentPane.add(durationComboBox);
-		
-		JButton recap = new JButton("Recapitulatif");
-		recap.setFont(new Font("Tahoma", Font.BOLD, 12));
-		recap.setBounds(50, 417, 127, 23);
-		contentPane.add(recap);
-		
-		JButton print = new JButton("Imprimer");
-		print.setFont(new Font("Tahoma", Font.BOLD, 12));
-		print.setBounds(241, 417, 111, 23);
-		contentPane.add(print);
-		
-		JButton diagramLines = new JButton("Graphique 2D");
-		diagramLines.setFont(new Font("Tahoma", Font.BOLD, 12));
-		diagramLines.setBounds(429, 417, 145, 23);
-		contentPane.add(diagramLines);
-		
-		JButton button = new JButton("Imprimer");
-		button.setFont(new Font("Tahoma", Font.BOLD, 12));
-		button.setBounds(665, 417, 111, 23);
-		contentPane.add(button);
+
 		try{
 			//connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/test_pds","root","");
 			statement=connection.createStatement();
 			result=statement.executeQuery("select * from duration");
 			
 			while( result.next()){
+				
+				durationComboBox.addItem(result.getString("duration"));
+			}
 
-				durationComboBox.addItem(result.getInt("duration")+" "+"ans");
+		}
+
+		catch (Exception e){
+			JOptionPane.showMessageDialog(null, e);
+			System.out.println(e);
+		}
+
+		JButton recap = new JButton("Recapitulatif");
+		recap.setFont(new Font("Tahoma", Font.BOLD, 12));
+		recap.setBounds(50, 417, 127, 23);
+		contentPane.add(recap);
+
+		JButton print = new JButton("t\u00E9l\u00E9charger");
+		print.setFont(new Font("Tahoma", Font.BOLD, 12));
+		print.setBounds(241, 417, 111, 23);
+		contentPane.add(print);
+
+		JButton diagramLines = new JButton("Graphique 2D");
+		diagramLines.setFont(new Font("Tahoma", Font.BOLD, 12));
+		diagramLines.setBounds(429, 417, 145, 23);
+		contentPane.add(diagramLines);
+
+		JButton button = new JButton("Imprimer");
+		button.setFont(new Font("Tahoma", Font.BOLD, 12));
+		button.setBounds(665, 417, 111, 23);
+		contentPane.add(button);
+
+		HashMap<String, Double> mapLoanTypeInsurance= new HashMap<>();
+		HashMap<String, Double> mapLoanTypeRate= new HashMap<>();
+		try{
+			//connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/test_pds","root","");
+			statement=connection.createStatement();
+			result=statement.executeQuery("select * from loanType ,insurance where insurance.id_insurance=loanType.id_insurance");
+			while( result.next()){
+				mapLoanTypeInsurance.put(result.getString("loanType.name"),result.getDouble("insurance.id_insurance"));
+				mapLoanTypeRate.put(result.getString("loanType.name"),result.getDouble("loanType.rate"));
 			}
 
 
@@ -172,26 +191,44 @@ public class InterClient extends JFrame {
 			JOptionPane.showMessageDialog(null, e);
 			System.out.println(e);
 		}
-		finally{
 
-			try{
-				statement.close();
-				result.close();
-				connection.close();
+		afficherMap(mapLoanTypeRate);
+		//System.out.println(mapLoanTypeInsurance.get("Prêt voiture").doubleValue());	
 
-			}catch(Exception e ){
-				JOptionPane.showMessageDialog(null, "Erreur");
 
-			}
-		}
-		/*
-		 * metho to simulate a loan with all arguments
-		 */
+
 
 		btnSimuler.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				JFrame jframe = new JFrame();
+				jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				double amount = Double.parseDouble(amountTextField.getText());
+				int duration=0;
+				if (durationComboBox.getSelectedIndex()==0){
+					duration =1;
+				}
+				else if (durationComboBox.getSelectedIndex()==1){
+					duration =5;
+				}
+				String loanType = (String)comboBoxLoanType.getSelectedItem();
+				double insurance=mapLoanTypeInsurance.get(loanType).doubleValue();
+				double rate=mapLoanTypeRate.get(loanType).doubleValue();
+				/*
+				 * le remboursement par défaut pour un client est de type trois.
+				 */
+				int reimbursementType = 3;
+				Frequency frequency = Frequency.Monthly;
 
-				System.out.println(comboBoxLoanType.getSelectedItem().toString());
+				LoanRepaymentPlan loanRepaymentPlan=new LoanRepaymentPlan(amount, rate, duration, insurance, reimbursementType, frequency);
+				loanRepaymentPlan.fillArray();
+				JTable arrayFrame = new JTable(new ShowArrayRestitution(loanRepaymentPlan.getData()));
+
+				jframe.getContentPane().add(new JScrollPane(arrayFrame), BorderLayout.CENTER);
+
+				jframe.pack();
+				jframe.setVisible(true);
+
+
 			}
 		});
 
@@ -213,5 +250,14 @@ public class InterClient extends JFrame {
 			}
 		});
 	}
+	public void afficherMap(HashMap<String, Double> map){
+		for(Entry<String, Double> entry : map.entrySet()) {
+			String cle = entry.getKey();
+			Double valeur = entry.getValue();
+			System.out.println("cle==>"+cle);
+			System.out.println("valeur==>"+valeur);
+
+		}
+	}
+
 }
-   
