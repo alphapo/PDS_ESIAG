7d596f340f6e2884a01dfdd0c881b49d74c85868
abda123 <vybzbalde@gmail.com>
2016-05-28 06:02:49 +0200

    Update ClientFactoryJson.java

--- rendu_R_3/client_package/client/tools/ClientFactoryJson.java
+++ rendu_R_3/client_package/client/tools/ClientFactoryJson.java
@@ -1,4 +1,4 @@
-package client.tools;
+package tools;
 
 import java.io.BufferedReader;
 import java.io.StringWriter;
@@ -9,8 +9,10 @@ import java.util.*;
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
 
-import client.beans.Client;
-import client.beans.ClientSimulation;
+import beans.Client;
+import beans.ClientSimulation;
+import beans.Duration;
+import client.DurationInterface;
 
 
 public class ClientFactoryJson {
@@ -43,62 +45,49 @@ public class ClientFactoryJson {
 	/*
 	 * this Json inserts data in the database
 	 */
-	public static String makeJSONsimulationForClient(ClientSimulation simulationData){
+	public static String makeJSONsimulationForClient(ClientSimulation clientSimulation){
 		
-	// Cette methode n'est pas terminée, il va falloir créer le methode 
-		Date date = new Date();
+		
+		System.out.println("id_user:"+ clientSimulation.getId_user());
+		System.out.println("day:"+ clientSimulation.getDay());
+		System.out.println("status:"+ clientSimulation.getStatus());
+		System.out.println("duration: "+clientSimulation.getDuration());
+		System.out.println("amount: "+ clientSimulation.getAmount());
+		System.out.println("rate: "+clientSimulation.getRate());
+		System.out.println("id_loanType: "+ clientSimulation.getId_loanType());
+		System.out.println("id_name: "+ clientSimulation.getId_name());
+		
+		
+		
 		StringWriter sw = new StringWriter();
 		JsonGenerator jsonGen = Json.createGenerator(sw);
 		jsonGen.writeStartObject()
 				.write("request","ADD_CLIENT_SIMULATION")
-				/*
-				.write("id_user", getUser())
-				.write("simulationDate", getDate())
-				.write("status", getStatus())
-				*/
-				.write("duration",simulationData.getDuration())
-				.write("amount",simulationData.getAmount())
-				/*.write("interest",getInterest())
-				.write("duration",getIdLoanType())
-				*/
+				.write("id_user",clientSimulation.getId_user())
+				.write("day", clientSimulation.getDay())
+				.write("status",clientSimulation.getStatus())
+				.write("duration",clientSimulation.getDuration())
+				.write("amount",clientSimulation.getAmount())
+				.write("rate",clientSimulation.getRate())
+				.write("id_loanType",clientSimulation.getId_loanType())
+				.write("id_name",clientSimulation.getId_name())
 				.writeEnd()
 				.close();
 		String jsonContent = sw.toString();
 		return jsonContent;
 	}
 	
-	/*
-	 * Json from ClientAdvisor 
-	 */
-	public static String makeJSONsimulationForClientAdvisor(ClientSimulation simulationData){
+	public static String makeJSONgetDurationData(){
 		
-	// Cette methode n'est pas terminée, il va falloir créer le methode. 
-	/*
-	 * ce Json prend plus de paramètres que celui du client
-	 */
-		Date date = new Date();
-		StringWriter sw = new StringWriter();
-		JsonGenerator jsonGen = Json.createGenerator(sw);
-		jsonGen.writeStartObject()
-				.write("request","ADD_ADVISOR_SIMULATION")
-				/*
-				.write("id_user", getUser())
-				.write("simulationDate", getDate())
-				.write("status", getStatus())
-				.write("freqency", setFreqency()
-				*/
-				.write("duration",simulationData.getDuration())
-				.write("amount",simulationData.getAmount())
-				/*.write("interest",getInterest())
-				.write("duration",getIdLoanType())
-				
-				*/
-				.write("frequency",simulationData.getFreqency())
-				
-				
-				.writeEnd()
-				.close();
-		String jsonContent = sw.toString();
-		return jsonContent;
-	}
+			StringWriter sw = new StringWriter();
+			JsonGenerator jsonGen = Json.createGenerator(sw);
+			jsonGen.writeStartObject()
+					.write("request","GET_DURATIONDATA")
+					.writeEnd()
+					.close();
+			String jsonContent = sw.toString();
+			return jsonContent;
+		}
+		
+	
 }
